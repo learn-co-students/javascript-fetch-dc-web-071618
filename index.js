4610b21d0ad6fd50a6c9578e949ecdfbd5eb5e60
john-raber <jrab224@gmail.com>
2018-08-31 08:58:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+//
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function () {
+//   console.log(xhr.response);
+// }
+//
+// xhr.onerror = function () {
+//   console.log('Booo');
+// }
+//
+// xhr.send();
+
+const token = 0b01478c58a25613b257221f9d05854cca6e1562;
+fetch('https://api.github.com/john-raber/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
